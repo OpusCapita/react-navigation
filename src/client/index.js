12b6dabb5c7a00b325b513144cd0e48549e40f0e
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-08-31 15:30:38 +0300

    Ready to publish

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,10 @@
 // Here should be CommonJS component exports.
 
 modules.exports = {
-
+  Menu: require('./components/Menu').default,
+  MenuIcon: require('./components/MenuIcon').default,
+  MenuIconsBar: require('./components/MenuIconsBar').default,
+  MenuLogo: require('./components/MenuLogo').default,
+  MenuSearch: require('./components/MenuSearch').default,
+  NavigationBar: require('./components/NavigationBar').default
 };
