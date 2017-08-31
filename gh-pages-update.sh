182af979078f2a65782f95848837c848234b9fd3
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-08-31 12:48:35 +0300

    Fix demo page build

--- gh-pages-update.sh
+++ gh-pages-update.sh
@@ -4,8 +4,8 @@ rm -rf .gh-pages-tmp lib  &&
 mkdir .gh-pages-tmp &&
 node node_modules/webpack/bin/webpack.js --config ./webpack.docs.config.js &&
 cp -R lib/* .gh-pages-tmp &&
-cp -R www/index.html .gh-pages-tmp &&
 mkdir .gh-pages-tmp/styles && cp -r node_modules/@opuscapita/styles/dist/npm/* .gh-pages-tmp &&
+cp -R www/index.html .gh-pages-tmp &&
 
 git checkout gh-pages &&
 git ls-files | grep -v -e "\(^\.gitignore$\|^\.gitattributes$\|^\.gh-pages-tmp$\)" | xargs rm -rf &&
