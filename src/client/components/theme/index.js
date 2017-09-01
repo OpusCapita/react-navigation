b2826073252a2a1169c1f5ef309b59c76ddc66a1
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-09-01 10:35:25 +0300

    Add themes to exports

---
+++ src/client/components/theme/index.js
@@ -0,0 +1,9 @@
+import opuscapitaDark from './opuscapita-dark';
+import opuscapitaLight from './opuscapita-light';
+import themePropTypes from './theme-prop-types';
+
+export default {
+  opuscapitaDark,
+  opuscapitaLight,
+  themePropTypes
+};
--- src/client/index.js
+++ src/client/index.js
@@ -6,5 +6,6 @@ module.exports = {
   MenuIconsBar: require('./components/MenuIconsBar').default,
   MenuLogo: require('./components/MenuLogo').default,
   MenuSearch: require('./components/MenuSearch').default,
-  NavigationBar: require('./components/NavigationBar').default
+  NavigationBar: require('./components/NavigationBar').default,
+  theme: require('./components/theme').default
 };
