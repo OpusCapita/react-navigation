f40b7bc43b9659fc979eb04baf6d26199ef26edc
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-09-11 12:16:23 +0300

    Fix exports

--- src/client/index.js
+++ src/client/index.js
@@ -2,10 +2,17 @@
 
 module.exports = {
   Menu: require('./components/Menu').default,
+  MenuAccount: require('./components/MenuAccount').default,
+  MenuAccountIcon: require('./components/MenuAccountIcon').default,
+  MenuDropdownGrid: require('./components/MenuDropdownGrid').default,
+  MenuDropdownList: require('./components/MenuDropdownList').default,
   MenuIcon: require('./components/MenuIcon').default,
   MenuIconsBar: require('./components/MenuIconsBar').default,
   MenuLogo: require('./components/MenuLogo').default,
   MenuSearch: require('./components/MenuSearch').default,
+  MenuSearchBar: require('./components/MenuSearchBar').default,
+  MenuSelect: require('./components/MenuSelect').default,
   NavigationBar: require('./components/NavigationBar').default,
+  Navigation: require('./components/Navigations').default,
   theme: require('./components/theme').default
 };
