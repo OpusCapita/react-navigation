f68f2aa94b9c7d8e6ac5673387234298bf0bf03e
Kirill Volkovich <kirill.volkovich@jcatalog.com>
2017-09-11 18:30:57 +0300

    Fix exports

--- src/client/components/theme/index.js
+++
@@ -1,9 +0,0 @@
-import opuscapitaDark from './opuscapita-dark';
-import opuscapitaLight from './opuscapita-light';
-import themePropTypes from './theme-prop-types';
-
-export default {
-  opuscapitaDark,
-  opuscapitaLight,
-  themePropTypes
-};
