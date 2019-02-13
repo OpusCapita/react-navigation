44d0638f9608fc68973861ea2bb436d10a495769
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2019-02-13 12:29:35 +0300

    Fix deploy demo to GitHub pages

--- scripts/gh-pages/build.sh
+++ scripts/gh-pages/build.sh
@@ -4,3 +4,4 @@ rm -rf .gh-pages-tmp &&
 node node_modules/@opuscapita/react-showroom-server/src/bin/showroom-scan.js src &&
 node node_modules/webpack/bin/webpack.js --config ./webpack.docs.config.js &&
 cp www/index.html .gh-pages-tmp
+cp -r static .gh-pages-tmp
